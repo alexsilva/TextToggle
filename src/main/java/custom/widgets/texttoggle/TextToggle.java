1d18549b14e28462051ed75a7fdaf57d9f5ed694
alex <geniofuturo@gmail.com>
2014-06-20 12:11:11 -0300

    git bug.

---
+++ src/main/java/custom/widgets/texttoggle/TextToggle.java
@@ -0,0 +1,26 @@
+package custom.widgets.texttoggle;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * TextToggle: like a bottom
+ */
+public class TextToggle extends LinearLayout {
+
+    public TextToggle(Context context) {
+        super(context);
+        initialize(context);
+    }
+
+    public TextToggle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context);
+    }
+
+    protected void initialize(Context context) {
+
+    }
+
+}
