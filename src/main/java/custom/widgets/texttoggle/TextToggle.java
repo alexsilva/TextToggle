ead4f6c3a8a4483c6b729934816eaf1497a4bd8d
alex <geniofuturo@gmail.com>
2014-06-20 13:15:31 -0300

    Implementando a view.

--- src/main/java/custom/widgets/texttoggle/TextToggle.java
+++ src/main/java/custom/widgets/texttoggle/TextToggle.java
@@ -1,13 +1,19 @@
 package custom.widgets.texttoggle;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * TextToggle: like a bottom
  */
-public class TextToggle extends LinearLayout {
+public class TextToggle extends LinearLayout implements View.OnClickListener {
+
+    int mColor;
 
     public TextToggle(Context context) {
         super(context);
@@ -20,7 +26,25 @@ public class TextToggle extends LinearLayout {
     }
 
     protected void initialize(Context context) {
+        isInEditMode();
 
+        LayoutInflater inflater = (LayoutInflater)
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.texttoggle, this, true);
+        findViewById(R.id.order_az_button).setOnClickListener(this);
+        mColor = getResources().getColor(R.color.before_selection);
     }
 
+    @Override
+    public void onClick(View v) {
+        int viewId = v.getId();
+        if (viewId == R.id.order_az_button) {
+            Resources res = getResources();
+            TextView text = (TextView) findViewById(R.id.order_az);
+            int before = res.getColor(R.color.after_selection);
+            int after = res.getColor(R.color.before_selection);
+            mColor = mColor == before ? after : before;
+            text.setTextColor(mColor);
+        }
+    }
 }
